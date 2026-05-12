87a1856e47935545aa7e6ba78d4378edc35a7067
rtgww <rtgww7777@woowahan.com>
2026-05-12 14:42:23 +0900

    fix langchain v1 imports

--- llm.py
+++ llm.py
@@ -1,14 +1,13 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, FewShotChatMessagePromptTemplate
+from langchain_core.retrievers import create_history_aware_retriever
+from langchain_core.retrievers.context_retriever import create_retrieval_chain
+from langchain_core.documents.compressor import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain.chains import create_retrieval_chain
-from langchain.chains import create_history_aware_retriever
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-
 from config import answer_examples
 
 store = {}
